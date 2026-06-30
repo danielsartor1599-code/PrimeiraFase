7ffa539d6fc86eadbd43b0d779c5fc613b21977a
danielsartor1599-code <danielsartor1599@gmail.com>
2026-06-30 19:16:56 -0300

    29/06/2026

---
+++ 06-FOR/script.js
@@ -0,0 +1,17 @@
+// 1. Utilize um for para calcular a soma dos números de 1 até 10 e exiba o resultado.
+
+// let soma = 0;
+
+// for(let i = 1; i <=10; i++){
+//     soma +=i;
+//     console.log(soma);
+// }
+
+// console.log("A soma total é: " + soma)
+
+// 2. Crie um programa que mostre a tabuada do número 5, de 5 × 1 até 5 × 10,
+
+for (let i = 1; i <= 10; i++){
+   
+    console.log(5 * ${i} = ${5 * i})
+}
